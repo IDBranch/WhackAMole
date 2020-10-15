badba372c6a7edf476f82894d27d490b2185e15a
Isaiah Branch <idbranch12@gmail.com>
2020-10-15 13:12:19 -0500

    Initial Commit

---
+++ WhackAMole/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhackAMole
+//
+//  Created by Branch, Isaiah D on 10/15/20.
+//  Copyright © 2020 Branch, Isaiah D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
